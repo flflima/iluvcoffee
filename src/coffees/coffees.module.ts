942172f3454bedd3f88db6bda94da04b1573119c
Felipe Lima <flf.felipe@gmail.com>
2021-06-13 18:13:52 -0300

    Mapeando entidades

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,12 @@
+import { Coffee } from './entities/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
